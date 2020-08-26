ffad01a284f87b61c19684dca46d989f8b5f6026
Kevin Elliott <kevin@welikeinc.com>
2020-08-26 00:15:20 -0700

    protobuf AirframesClientOutputFrame: generate language bindings

--- protobuf/compile.sh
+++ protobuf/compile.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 protoc --include_imports --descriptor_set_out=airframes_client_output_frame.desc airframes_client_output_frame.proto
+protoc --cpp_out=cpp airframes_client_output_frame.proto
 protoc --dart_out=dart airframes_client_output_frame.proto
 protoc --js_out=js airframes_client_output_frame.proto
+protoc --python_out=python airframes_client_output_frame.proto
 protoc --ruby_out=ruby airframes_client_output_frame.proto
