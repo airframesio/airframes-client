3ef2db85d5f898ef4a85b76d2861420f28de4434
Kevin Elliott <kevin@welikeinc.com>
2020-08-26 17:24:42 -0700

    afc.py: improvements to parallel processing and threading, output harnessing, safe shutdown

--- scripts/afc.py
+++ scripts/afc.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 import multiprocessing
+from multiprocessing.managers import BaseManager, SyncManager
 import subprocess
 import shlex
+import logging
+import sys
+import threading
+import os
+import json
+from time import sleep
 
 class ClientInput:
   input_type = 'unknown'
@@ -18,6 +25,9 @@ class ClientInput:
   def full_command(self):
     return '{command_path} {params}'.format(command_path=self.command_path(), params=self.params())
 
+  def version(self):
+    return None
+
 class AcarsdecClientInput(ClientInput):
   input_type = 'acarsdec'
   command = 'acarsdec'
@@ -27,11 +37,20 @@ class AcarsdecClientInput(ClientInput):
   def params(self):
     return '-v -o 4 -g {gain} -i {ident} -r {receiver} {frequencies}'.format(ident=self.ident, receiver=self.receiver, gain=self.gain, frequencies=self.frequencies)
 
+  def system_version(self):
+    return os.system('acarsdec 2>&1 | head -2 | tail -1 | cut -d " " -f 2').strip()
+
 class Vdlm2decClientInput(ClientInput):
   input_type = 'vdlm2dec'
   command = 'vdlm2dec'
-  gain = 400
-  frequencies = ''
+  gain = 200
+  frequencies = '136.650 136.800 136.975'
+
+  def params(self):
+    return '-v -J -G -E -U -g {gain} -i {ident} -j 0.0.0.0:5555 -r {receiver} {frequencies}'.format(ident=self.ident, receiver=self.receiver, gain=self.gain, frequencies=self.frequencies)
+
+  def system_version(self):
+    return os.system('vdlm2dec -h 2>&1 | head -2 | tail -1 | cut -d " " -f 2').strip()
 
 class Dumpvdl2ClientInput(ClientInput):
   input_type = 'dumpvdl2'
@@ -42,39 +61,103 @@ class Dumpvdl2ClientInput(ClientInput):
   def params(self):
     return '--rtlsdr {receiver} --gain {gain} {frequencies} --output decoded:json:file:path=-'.format(receiver=self.receiver, gain=self.gain, frequencies=self.frequencies)
 
+  def system_version(self):
+    return os.system('dumpvdl2 --version').strip()
 
-def start_client_input(client_input):
+
+class LogPipe(threading.Thread):
+
+    def __init__(self, level):
+        """Setup the object with a logger and a loglevel
+        and start the thread
+        """
+        threading.Thread.__init__(self)
+        self.daemon = False
+        self.level = level
+        self.fdRead, self.fdWrite = os.pipe()
+        self.pipeReader = os.fdopen(self.fdRead)
+        self.start()
+
+    def fileno(self):
+        """Return the write file descriptor of the pipe
+        """
+        return self.fdWrite
+
+    def run(self):
+        """Run the thread, logging everything.
+        """
+        for line in iter(self.pipeReader.readline, ''):
+            logging.log(self.level, line.strip('\n'))
+
+        self.pipeReader.close()
+
+    def close(self):
+        """Close the write end of the pipe.
+        """
+        os.close(self.fdWrite)
+
+
+def start_client_input(queue, client_input):
   """Start and handle a ClientInput"""
-  print('Started {}.'.format(client_input.input_type))
-  print(client_input.full_command())
-  process = subprocess.Popen(shlex.split(client_input.full_command()),
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
-  )
-  while True:
-    output = process.stdout.readline()
-    print('[{}] {}'.format(client_input.input_type, output.strip()))
-    return_code = process.poll()
-    if return_code is not None:
-      print('[{}] Exited: {}'.format(client_input.input_type, return_code))
-      for output in process.stdout.readlines():
-        print('[{}] {}'.format(client_input.input_type, output.strip()))
-      for output in process.stderr.readlines():
-        print('[{}] Error: {}'.format(client_input.input_type, output.strip()))
-      break
+  log_file = '/var/log/airframes/{}.log'.format(client_input.input_type)
+  logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO, handlers=[logging.FileHandler(log_file)], datefmt='%Y-%m-%d %H:%M:%S')
+  logpipe = LogPipe(logging.INFO)
+
+  logging.info('Started {}.'.format(client_input.input_type))
+  with subprocess.Popen(shlex.split(client_input.full_command()),
+    stdout=subprocess.PIPE, stderr=logpipe, universal_newlines=True) as process:
+    try:
+      while True:
+        stdout = process.stdout.readline().strip()
+        frame = {
+          'source': {
+            'app': {
+              'name': client_input.input_type
+            }
+          },
+          'payload': stdout
+        }
+        json_string = json.dumps(frame)
+        print('[{}] {}'.format(client_input.input_type, json_string))
+        queue.put(json_string)
+
+        return_code = process.poll()
+        if return_code is not None:
+          print('[{}] Exited: {}'.format(client_input.input_type, return_code))
+          for output in process.stdout.readlines():
+            print('[{}] {}'.format(client_input.input_type, output.strip()))
+          break
+    except KeyboardInterrupt:
+      pass
+    except:
+      print('Unexpected Exception: {}. Forcefully shutting down child exec process for {}'.format(sys.exc_info()[0], client_input.input_type))
+      logpipe.close()
+      process.kill()
+    else:
+      print('Sanely shutting down {}', client_input.input_type)
+      logpipe.close()
 
 if __name__ == '__main__':
   inputs = []
 
-  input1 = AcarsdecClientInput()
-  input1.receiver = "0"
-  input1.ident = "KE-KMHR1"
-  input1.frequencies = "131.550 131.525 131.725 130.025 130.450 131.125"
-  inputs.append(input1)
+  with AcarsdecClientInput() as inp:
+    inp.receiver = "0"
+    inp.gain = 48
+    inp.ident = "KE-KMHR1"
+    inp.frequencies = "131.550 131.525 131.725 130.025 130.450 131.125"
+    inputs.append(inp)
 
-  input2 = Dumpvdl2ClientInput()
-  input2.receiver = "1"
-  input2.frequencies = "136650000 136800000 136975000"
-  inputs.append(input2)
+  with Vdlm2decClientInput() as inp:
+    inp.receiver = "0"
+    inp.gain = 200
+    inp.ident = "KE-KMHR2"
+    inp.frequencies = "136.650 136.800 136.975"
+    inputs.append(inp)
+
+  # with Dumpvdl2ClientInput() as inp:
+  #   inp.receiver = "1"
+  #   inp.frequencies = "136650000 136800000 136975000"
+  #   inputs.append(inp)
 
   print('Client Inputs')
   for client_input in inputs:
@@ -82,7 +165,23 @@ if __name__ == '__main__':
   print()
 
   processes = []
-  for client_input in inputs:
-    p = multiprocessing.Process(target=start_client_input, args=(client_input,))
-    processes.append(p)
-    p.start()
+  try:
+    queue = multiprocessing.Queue()
+    for client_input in inputs:
+      p = multiprocessing.Process(target=start_client_input, args=(queue, client_input,))
+      p.name = client_input.input_type
+      processes.append(p)
+      p.start()
+    while True:
+      json_string = queue.get()
+      print("[MAIN] child sent me: {}".format(json_string))
+    for p in processes:
+      p.join()
+  except KeyboardInterrupt:
+    print('Keyboard interrupt, shutting down processes...')
+    sys.stdout.flush()
+    for p in processes:
+      print('Shutting down {}'.format(p.name))
+      sys.stdout.flush()
+      p.terminate()
+      p.join()
