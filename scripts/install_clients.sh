7006e41c0cc4a60da4e67ffbd75ace3e40687184
Kevin Elliott <kevin@welikeinc.com>
2020-09-12 07:15:20 -0700

    Minor changes to the install_clients script

--- scripts/install_clients.sh
+++ scripts/install_clients.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+ORIGINAL_PATH=`pwd`
 SOURCE_PATH=~/source
+mkdir -p ${SOURCE_PATH}
 
 echo "[Airframes] Installing system dependencies"
 apt update
 apt install -y build-essential cmake git libglib2.0-dev pkg-config librtlsdr-dev libsqlite3-dev libprotobuf-c-dev libzmq3-dev
+echo
 
 echo "[Airframes] Pulling down sources"
-mkdir -p ${SOURCE_PATH}
 cd ${SOURCE_PATH}
 git clone https://github.com/airframesio/airframes-client.git
 git clone https://github.com/TLeconte/acarsdec.git
@@ -15,6 +17,7 @@ git clone https://github.com/szpajder/libacars
 git clone https://github.com/romanbsd/statsd-c-client.git
 git clone https://github.com/airframesio/dumpvdl2.git
 git clone https://github.com/TLeconte/vdlm2dec.git
+echo
 
 echo "[Airframes] Building libacars"
 cd ${SOURCE_PATH}/libacars
@@ -22,6 +25,7 @@ mkdir build
 cd build
 cmake ../
 make && make install && ldconfig
+echo
 
 echo "[Airframes] Building acarsdec"
 cd ${SOURCE_PATH}/acarsdec
@@ -29,10 +33,12 @@ mkdir build
 cd build
 cmake .. -Drtl=ON
 make && make install
+echo
 
 echo "[Airframes] Building statsd-c-client"
 cd ${SOURCE_PATH}/statsd-c-client
 make && make install && ldconfig
+echo
 
 echo "[Airframes] Building dumpvdl2"
 cd ${SOURCE_PATH}/dumpvdl2
@@ -40,6 +46,7 @@ mkdir build
 cd build
 cmake ../
 make && make install
+echo
 
 echo "[Airframes] Building vdlm2dec"
 cd ${SOURCE_PATH}/vdlm2dec
@@ -47,14 +54,28 @@ mkdir build
 cd build
 cmake .. -Drtl=ON
 make && make install
+echo
 
 echo "[Airframes] Installing airframes-client"
 cd ${SOURCE_PATH}/airframes-client
 if [ ! -f "/boot/airframes.json" ]; then
   cp json/airframes.json /boot/airframes.json
 fi
-apt install -y python3
+cp debian/adc.service /etc/systemd/system
+systemctl enable adc
+apt install -y python3 python3-pip
 mkdir -p /usr/local/airframes
 cp -rf scripts/adc /usr/local/airframes
 cd /usr/local/airframes/adc
 pip3 install -r requirements.txt
+echo
+
+echo "[Airframes] Done!"
+echo
+cd $ORIGINAL_PATH
+
+echo "[Airframes] Next steps:"
+echo
+echo "            1. Customize /boot/airframes.json"
+echo "            2. Restart the service: systemctl restart adc"
+echo
